1edabe56207d4d235fca0e2eae40fc53e4dd0b7c
jimeest16 <jimenasalasthames@gmail.com>
2025-06-30 16:38:50 -0600

    Cambios solo en el historial de vehiculos y en rutas mias

--- src/main/java/com/mycompany/proyecto2_progra2/data/RepuestosData.java
+++ src/main/java/com/mycompany/proyecto2_progra2/data/RepuestosData.java
@@ -30,9 +30,12 @@ public class RepuestosData {
     private Element raiz;
     private String rutaDocumento;
     // => Se tiene que cambiar la ruta
+    
     //public static final String RUTA_ARCHIVO = "C:\\Repositorios\\Proyecto2-Programación2\\Original\\TallerMecanico-Proyecto2\\src\\main\\java\\com\\mycompany\\proyecto2_progra2\\xml\\repuestos.xml";
-    public static final String RUTA_ARCHIVO = "C:\\Users\\jeffr\\OneDrive\\Documentos\\Proyecto2-Progra2\\TallerMecanico-Proyecto2\\src\\main\\java\\com\\mycompany\\proyecto2_progra2\\xml\\repuestos.xml";
-    //public static final String RUTA_ARCHIVO = "C:\\Users\\jimen\\OneDrive\\Escritorio\\ProyectoProgra\\TallerMecanico-Proyecto2\\xml\\repuestos.xml";
+    //public static final String RUTA_ARCHIVO = "C:\\Users\\jeffr\\OneDrive\\Documentos\\Proyecto2-Progra2\\TallerMecanico-Proyecto2\\src\\main\\java\\com\\mycompany\\proyecto2_progra2\\xml\\repuestos.xml";
+    //Jime 
+     public static final String RUTA_ARCHIVO = "C:\\Users\\jimen\\OneDrive\\Escritorio\\TallerMecanico\\TallerMecanico-Proyecto2\\src\\main\\java\\com\\mycompany\\proyecto2_progra2\\xml\\repuestos.xml";
+
 
     public RepuestosData() throws IOException, JDOMException {
         File archivo = new File(RUTA_ARCHIVO);
